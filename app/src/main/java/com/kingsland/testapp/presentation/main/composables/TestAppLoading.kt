e5e80707a7abbfa66a5e992efd74eb7ca7dbd428
William <williamkingsley2019@outlook.com>
2023-09-27 16:34:07 -0600

    Spacing loading indicator

--- app/src/main/java/com/kingsland/testapp/presentation/main/composables/TestAppLoading.kt
+++ app/src/main/java/com/kingsland/testapp/presentation/main/composables/TestAppLoading.kt
@@ -1,23 +1,31 @@
 package com.kingsland.testapp.presentation.main.composables
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kingsland.testapp.R
 
 @Composable
 internal fun TestAppLoading() {
     Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         CircularProgressIndicator()
         Text(
+            modifier = Modifier.padding(vertical = 5.dp),
             text = stringResource(R.string.loading_data),
-            fontSize = 14.sp
+            fontSize = 16.sp
         )
     }
 }
