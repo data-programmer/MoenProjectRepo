db511abef5ec2c24c74359a84819f8bf2c75772a
William <williamkingsley2019@outlook.com>
2023-09-27 16:10:20 -0600

    Cleaning up item card

--- app/src/main/java/com/kingsland/testapp/presentation/main/composables/TestAppItem.kt
+++ app/src/main/java/com/kingsland/testapp/presentation/main/composables/TestAppItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -13,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -24,6 +26,7 @@ fun TestAppItem(item: Item) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .height(350.dp)
             .padding(5.dp),
         shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation(8.dp)
@@ -40,13 +43,18 @@ fun TestAppItem(item: Item) {
                     text = item.capitalTitle ?: item.title,
                     fontSize = 22.sp
                 )
+                // TODO: The date can be parsed into a more readable format
                 Text(
                     text = item.date ?: stringResource(R.string.unknown_date),
                     fontSize = 22.sp
                 )
             }
             // A placeholder would normally be provided here
+            // You can also use a "loading" placeholder
             AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
                 model = item.img,
                 contentDescription = stringResource(R.string.image_image)
             )
@@ -56,4 +64,18 @@ fun TestAppItem(item: Item) {
             )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TestAppItemPreview() {
+    TestAppItem(
+        item = Item(
+            capitalTitle = "This is a title",
+            date = "9/30/2022",
+            description = "This is the description of the item.",
+            img = "",
+            title = "This is another title"
+        )
+    )
 }
